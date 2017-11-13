82f3fd9af3b16437ec8ca3ac069b8f50d651424c
Etienne <etienner37@gmail.com>
2017-11-13 16:41:16 +0100

    debut de routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,27 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { CalendarModule } from 'angular-calendar';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './features/welcome/welcome.component';
+import { IntegrationComponent } from './features/integration/integration.component';
+
+const appRoutes: Routes = [
+   { path: 'welcome', component: WelcomeComponent },
+   { path: 'integration', component: IntegrationComponent },
+   {path: '', redirectTo: '/welcome', pathMatch: 'full'}
+ ];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+   RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     BrowserAnimationsModule,
     SharedModule,
