37942664adc024a1e09892f0c5bc0192ee5b901a
Rutabadev <etienner37@gmail.com>
2017-11-20 15:17:25 +0100

    active button

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,49 +1,55 @@
 import { Component, OnInit, Renderer2, Renderer } from '@angular/core';
 import { OverlayContainer } from '@angular/cdk/overlay';
-import {Element} from './element'
+import { Element } from './element'
 import { ELEMENTS } from './mocks'
 
 
 @Component({
-   selector: 'app-header',
-   templateUrl: './header.component.html',
-   styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-   checked: boolean;
-   overlayContainer: OverlayContainer;
-   eventCalls: any;
-   elements: Element[]
+    checked: boolean;
+    overlayContainer: OverlayContainer;
+    eventCalls: any;
+    elements: Element[]
 
-   constructor(overlayContainer: OverlayContainer, private renderer: Renderer, private renderer2: Renderer2) {
-      this.overlayContainer = overlayContainer;
-   }
+    constructor(overlayContainer: OverlayContainer, private renderer: Renderer, private renderer2: Renderer2) {
+        this.overlayContainer = overlayContainer;
+    }
 
-   ngOnInit() {
-      if (localStorage.getItem('darkTheme') === 'true') {
-         this.setDarkTheme();
-         this.checked = true;
-      }
-      this.elements = ELEMENTS;
-   }
+    ngOnInit() {
+        if (localStorage.getItem('darkTheme') === 'true') {
+            this.setDarkTheme();
+            this.checked = true;
+        }
+        this.elements = ELEMENTS;
+    }
 
-   darkTheme(event) {
-      if (event.checked) {
-         this.setDarkTheme();
-         localStorage.setItem('darkTheme', 'true');
-      } else {
-         this.setLightTheme();
-         localStorage.setItem('darkTheme', 'false');
-      }
-   }
+    darkTheme(event) {
+        if (event.checked) {
+            this.setDarkTheme();
+            localStorage.setItem('darkTheme', 'true');
+        } else {
+            this.setLightTheme();
+            localStorage.setItem('darkTheme', 'false');
+        }
+    }
 
-   private setDarkTheme() {
-      this.overlayContainer.getContainerElement().classList.add('unicorn-dark-theme');
-      this.renderer2.addClass(document.body, 'unicorn-dark-theme');
-   }
-   private setLightTheme() {
-      this.overlayContainer.getContainerElement().classList.remove('unicorn-dark-theme');
-      this.renderer2.removeClass(document.body, 'unicorn-dark-theme');
-   }
+    private setDarkTheme() {
+        this.overlayContainer.getContainerElement().classList.add('unicorn-dark-theme');
+        this.renderer2.addClass(document.body, 'unicorn-dark-theme');
+    }
+    private setLightTheme() {
+        this.overlayContainer.getContainerElement().classList.remove('unicorn-dark-theme');
+        this.renderer2.removeClass(document.body, 'unicorn-dark-theme');
+    }
 
+    activate(activatedElement) {
+        this.elements.forEach(element => {
+            element.isActive = false;
+        });
+        activatedElement.isActive = true;
+    }
 }
