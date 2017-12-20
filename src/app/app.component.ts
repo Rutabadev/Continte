2eba41724ca95459f645bde810b48ff89e61e20f
Rutabadev <etienner37@gmail.com>
2017-12-20 14:33:09 +0100

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { ELEMENTS } from './core/header/mocks';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent implements OnInit {
