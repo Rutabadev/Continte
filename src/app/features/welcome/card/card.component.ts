0a444ca967554ab0507a58696a4a77bbf2114783
Etienne <etienner37@gmail.com>
2017-12-20 21:17:40 +0100

    pulse on hover

--- src/app/features/welcome/card/card.component.ts
+++ src/app/features/welcome/card/card.component.ts
@@ -2,26 +2,29 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Content } from '../content';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+   selector: 'app-card',
+   templateUrl: './card.component.html',
+   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
-  iconBkp: string;
-  @Input() content: Content;
+   pulse: boolean;
+   iconBkp: string;
+   @Input() content: Content;
 
-  constructor() { }
+   constructor() { }
 
-  ngOnInit() {
-    this.iconBkp = this.content.icon;
-  }
+   ngOnInit() {
+      this.iconBkp = this.content.icon;
+   }
 
-  switchIcon() {
-    if (this.content.icon === 'add') {
-      this.content.icon = this.iconBkp;
-    } else {
-      this.content.icon = 'add';
-    }
-  }
+   switchIcon() {
+      if (this.content.icon === 'add') {
+         this.content.icon = this.iconBkp;
+         this.pulse = false;
+      } else {
+         this.content.icon = 'add';
+         this.pulse = true;
+      }
+   }
 
 }
