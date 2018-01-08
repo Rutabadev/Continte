d56970e698d604deb6a18e6b5c79bb0a3cdbf2cd
Etienne <etienner37@gmail.com>
2018-01-08 12:03:17 +0100

    todos update

--- src/app/features/todos/todos.component.ts
+++ src/app/features/todos/todos.component.ts
@@ -4,71 +4,95 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from '@angular/
 import { Todo } from './todo';
 
 @Component({
-  selector: 'app-todos',
-  templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss'],
-  providers: [TodosService]
+   selector: 'app-todos',
+   templateUrl: './todos.component.html',
+   styleUrls: ['./todos.component.scss'],
+   providers: [TodosService]
 })
 export class TodosComponent implements OnInit {
-  loading = true;
-  tasks: any[];
-  todosService: TodosService;
+   loading = true;
+   tasks: any[];
+   todosService: TodosService;
 
-  constructor(todosService: TodosService, public dialog: MatDialog, public snackBar: MatSnackBar) {
-    this.todosService = todosService;
-  }
+   constructor(todosService: TodosService, public dialog: MatDialog, public snackBar: MatSnackBar) {
+      this.todosService = todosService;
+   }
 
-  ngOnInit() {
-    this.todosService.getAllTasks().subscribe(tasks => {
-      this.tasks = tasks;
-      this.loading = false;
-    }
-    );
-  }
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(AddTodoDialogComponent, {
-      width: '250px',
-      data: { name: null }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.todosService.createTask(result);
-        this.tasks.push(result);
-        this.snackBar.open('Task created', null, {
-          duration: 2000
-        });
+   ngOnInit() {
+      this.todosService.getAllTasks().subscribe(tasks => {
+         this.tasks = tasks;
+         this.loading = false;
       }
-    });
-  }
+      );
+   }
 
-  delete(id) {
-    this.todosService.deleteTask(id).subscribe(result => {
-      this.tasks = this.tasks.filter(task => (task._id !== id));
-      this.snackBar.open('Task deleted', null, {
-        duration: 2000
+   openDialog(task): void {
+      let options;
+
+      if (task) {
+         options = {
+            width: '250px',
+            data: {
+               name: task.name,
+               isUpdate: true
+            }
+         };
+      } else {
+         options = {
+            width: '250px',
+            data: { name: null }
+         };
+      }
+
+      const dialogRef = this.dialog.open(AddTodoDialogComponent, options);
+
+      dialogRef.afterClosed().subscribe(result => {
+         // Check if the dialog as been completed or just exited
+         if (result) {
+            // Check if we need to create or update a task
+            if (result.isUpdate) {
+               this.todosService.updateTask(result);
+               this.tasks[this.tasks.indexOf(task)] = result;
+               this.snackBar.open('Task updated', null, {
+                  duration: 2000
+               });
+            } else {
+               this.todosService.createTask(result);
+               this.tasks.push(result);
+               this.snackBar.open('Task created', null, {
+                  duration: 2000
+               });
+            }
+         }
       });
-    });
-  }
+   }
 
-  update(task) {
-    alert('Not implemented yet\n' + JSON.stringify(task));
-  }
+   delete(id) {
+      this.todosService.deleteTask(id).subscribe(result => {
+         this.tasks = this.tasks.filter(task => (task._id !== id));
+         this.snackBar.open('Task deleted', null, {
+            duration: 2000
+         });
+      });
+   }
+
+   update(task) {
+      alert('Not implemented yet\n' + JSON.stringify(task));
+   }
 
 }
 
 @Component({
-  selector: 'app-add-todo-dialog',
-  templateUrl: './add-todo-dialog.component.html',
-  styleUrls: ['./add-todo-dialog.component.scss']
+   selector: 'app-add-todo-dialog',
+   templateUrl: './add-todo-dialog.component.html',
+   styleUrls: ['./add-todo-dialog.component.scss']
 })
 export class AddTodoDialogComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<AddTodoDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+   constructor(public dialogRef: MatDialogRef<AddTodoDialogComponent>,
+      @Inject(MAT_DIALOG_DATA) public data: any) { }
 
-  ngOnInit() {
-  }
+   ngOnInit() {
+   }
 
 }
